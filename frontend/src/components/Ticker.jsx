e7aaf9450cf130496b439f4a296207e657badbb3
Alen Shibu <your-email@example.com>
2026-05-23 10:36:17 +0530

    ticker

---
+++ frontend/src/components/Ticker.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import '../styles/Ticker.css'
+
+function Ticker() {
+    const words = [
+        "Indoor Advertising", 
+        "Gyms", 
+        "Cafés", 
+        "Salons", 
+        "Clinics", 
+        "High Impact", 
+        "Hyper Local"
+    ];
+  return (
+    <div className='ticker-wrapper'>
+        <div className="ticker-track">
+            {/* First instance */}
+            <div className="ticker-group">
+                {
+                    words.map((word,idx)=> (
+                        <React.Fragment key={`first-${idx}`}>
+                            <span className='ticker-item'>{word}</span>
+                            <span className='ticker-dot'>•</span>
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+            {/* Second instance for illusion */}
+            <div className="ticker-group" aria-hidden="true">
+                {
+                    words.map((word,idx)=> (
+                        <React.Fragment key={`second-${idx}`}>
+                            <span className='ticker-item'>{word}</span>
+                            <span className='ticker-dot'>✦</span>
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Ticker
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSection from '../sections/HeroSection'
+import Ticker from '../components/Ticker'
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Ticker />
     </div>
   )
 }
