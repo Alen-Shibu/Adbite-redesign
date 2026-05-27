a2155c821c6de1de07fb803c58a745a56b846aa2
Alen Shibu <your-email@example.com>
2026-05-27 22:13:54 +0530

    Main accordion done

--- frontend/src/components/adminComponents/CMS.jsx
+++ frontend/src/components/adminComponents/CMS.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './CMS.module.css'
+import axios from '../../api/axios.js'
 
 const CMS = () => {
   const [openIndex, setOpenIndex] = useState(null)
+  const [districts, setDistricts] = useState([])
+
+  useEffect(()=>{
+    const getLocations = async()=>{
+      try {
+        const res = await axios.get('/locations');
+        setDistricts(res.data)
+      } catch (error) {
+        console.log('Error in fetching locations',error)
+      }
+    }
+    getLocations();
+  }
+  ,[])
 
   const toggleAccordion = (index) =>{
     if(openIndex===index){
@@ -19,69 +34,73 @@ const CMS = () => {
 
       <div className={styles.page_top}>
         <div className={styles.search_bar}> 
-          <svg className={styles.search_icon} viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+          <svg className={styles.search_icon} viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
           <input type="text" placeholder='Search' className={styles.search_input} />
         </div>
           <button className={styles.add_btn}>
-          <svg className={styles.plus_icon} viewBox="0 0 24 24" fill="none" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+          <svg className={styles.plus_icon} viewBox="0 0 24 24" fill="none" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
             Add District</button>
       </div>
 
-    <div className={styles.table_wrap}>
-    <table>
-      <thead>
-        <tr>
-          <th style={{width:'32px'}}></th>
-          <th>District</th>
-          <th>Description</th>
-          <th>Venues</th>
-          <th></th>
-        </tr>
-      </thead>
+      <div className={styles.table_wrap}>
+          <table>
+            <thead>
+              <tr>
+                <th style={{width:'32px'}}></th>
+                <th>District</th>
+                <th>Description</th>
+                <th>Venues</th>
+                <th></th>
+              </tr>
+            </thead>
 
-      <tbody>
-        <tr>
-          <td><button  onClick={()=> toggleAccordion(0)} className={`${styles.btn_ghost} ${styles.btn_sm} ${openIndex===0 && styles.rotate}`}><svg className={styles.btn_icon} viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <polyline points="9 18 15 12 9 6"></polyline>
-          </svg></button></td>
-          <td className={styles.td_name}>Ernakulam</td>
-          <td className={styles.td_description}>High commercial footfall — malls, gyms, caoasuyshshshshshshshshshshshshshshshshshshshshshshshshshshlinics.</td>
-          <td className={styles.venue}><span className={`${styles.badge} ${styles.badge_rust}`}>2 venues</span></td>
-          <td className={styles.td_buttons}>
-            <button className={`${styles.btn_ghost} ${styles.btn_sm}`}>Edit</button>
-            <button className={`${styles.btn_danger} ${styles.btn_sm}`}>Delete</button>
-          </td>
-        </tr>
-        <tr className={`${styles.venues_row} ${openIndex===0 && styles.active}`}>
-            <td colSpan={5}> 
-              <div className={styles.venue_label}>Venues in Ernakulam</div>
-              <div className={styles.venue_pills}>
-                <div className={styles.venue_pill}>
-                  <span className={styles.venue_dot}></span>
-                  <span>Gold's Gym Kakkanad</span>
-                  <span>10.0130°N, 76.3490°E</span>
-                </div>
-                <div className={styles.venue_pill}>
-                  <span className={styles.venue_dot}></span>
-                  <span>Gold's Gym Kakkanad</span>
-                  <span>10.0130°N, 76.3490°E</span>
-                </div>
-                <div className={styles.venue_pill}>
-                  <span className={styles.venue_dot}></span>
-                  <span>Gold's Gym Kakkanad</span>
-                  <span>10.0130°N, 76.3490°E</span>
-                </div>
-              </div>
-            </td>
-        </tr>
-       
-      </tbody>
-    </table>
-    </div>
-
+      {
+        districts.map((disctrict,idx)=>(
+          <React.Fragment key={idx}>
+            <tbody>
+              <tr>
+                <td><button  onClick={()=> toggleAccordion(idx)} className={`${styles.btn_ghost} ${styles.btn_sm} ${openIndex===idx && styles.rotate}`}><svg className={styles.btn_icon} viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <polyline points="9 18 15 12 9 6"></polyline>
+                </svg></button></td>
+                <td className={styles.td_name}>{disctrict.district}</td>
+                <td className={styles.td_description}>{disctrict.description}</td>
+                <td className={styles.venue}><span className={`${styles.badge} ${styles.badge_rust}`}>{disctrict.venues.length}</span></td>
+                <td className={styles.td_buttons}>
+                  <button className={`${styles.btn_ghost} ${styles.btn_sm}`}>Edit</button>
+                  <button className={`${styles.btn_danger} ${styles.btn_sm}`}>Delete</button>
+                </td>
+              </tr>
+              <tr className={`${styles.venues_row} ${openIndex===idx && styles.active}`}>
+                  <td colSpan={5}> 
+                    <div className={styles.venue_label}>Venues in {disctrict.district}</div>
+                    {
+                      disctrict.venues.map((venue,idx)=>(
+                        <div className={styles.venue_pills} key={idx}>
+                          <div className={styles.venue_pill}>
+                            <span className={styles.venue_dot}></span>
+                            <span>{venue.name}</span>
+                            <span>{venue.lat},{venue.lng}</span>
+                          </div>
+                        </div>
+                      ))
+                    }
+                  </td>
+              </tr>
+            
+            </tbody>
+          </React.Fragment>
+        ))
+      }
+          </table>
+          </div>
 
     </main>
   )
 }
 
 export default CMS
+
+
+
+
+
